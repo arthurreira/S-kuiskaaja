8a3f440ff3f0184a100d570417b3d93ea8281a55
Arthur Ferreira <87419255+Naroshh@users.noreply.github.com>
2024-03-24 20:05:57 +0200

    Update sketcH_dht11.ino
    suomeksi

--- Esp32 arduivno IDE/sketcH_dht11/sketcH_dht11.ino
+++ Esp32 arduivno IDE/sketcH_dht11/sketcH_dht11.ino
@@ -1,73 +1,73 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include "DHT.h"
+#include <WiFi.h>               // WiFi-kirjasto ESP32:lle
+#include <HTTPClient.h>         // HTTP-asiakaskirjasto HTTP-pyynnön tekemiseen
+#include "DHT.h"                // DHT-anturikirjasto
 
-const char* ssid = "mywifi";      // Enter your WiFi SSID here
-const char* password = "mypassword";  // Enter your WiFi password here
+const char* ssid = "mywifi";        // WiFi-verkkosi SSID
+const char* password = "mypassword"; // WiFi-verkkosi salasana
 
-// DHT Sensor
-#define DHTPIN 5
-#define DHTTYPE DHT11  // Change this if you're using a different DHT sensor
+// DHT-anturi
+#define DHTPIN 5                    // DHT-anturin data-pinni
+#define DHTTYPE DHT11               // Käytetty DHT-anturityyppi
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE);           // Luodaan DHT-anturin objekti
 
-const char* serverIP = "xxx.xxx.xxx.xxx";                       // Use the IP of your WAMP server
-const char* endpoint = "/Weather-app-IOT/insert_data_dht11.php";  // Endpoint for the PHP script
+const char* serverIP = "xxx.xxx.xxx.xxx";                        // WAMP-palvelimesi IP-osoite
+const char* endpoint = "/Weather-app-IOT/insert_data_dht11.php"; // PHP-skriptin loppupiste
 
-
-#define LED_PIN 13  // LED pin
+#define LED_PIN 13                  // LEDin pinni
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT); // Initialize LED pin
+  Serial.begin(115200);             // Alustetaan sarjaportti
 
-  pinMode(DHTPIN, INPUT);
-  dht.begin();
+  pinMode(LED_PIN, OUTPUT);         // Alustetaan LEDin pinni
 
-  Serial.println("Connecting to ");
+  pinMode(DHTPIN, INPUT);           // Asetetaan DHT-anturin pinni syötteenä
+  dht.begin();                      // Alustetaan DHT-anturi
+
+  Serial.println("Yhdistetään verkkoon ");
   Serial.println(ssid);
 
-  // Connect to Wi-Fi
+  // Yhdistetään WiFi-verkkoon
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");
+  Serial.println("WiFi-yhteys muodostettu..!");
+  Serial.print("Saimme IP:n: ");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();   // Luetaan lämpötila DHT-anturilta
+  float humidity = dht.readHumidity();         // Luetaan ilmankosteus DHT-anturilta
 
-  if (isnan(temperature) || isnan(humidity)) {
-    Serial.println("Failed to read from DHT sensor!");
+  if (isnan(temperature) || isnan(humidity)) { // Tarkistetaan, onko lukemat kelvollisia
+    Serial.println("DHT-anturilta lukeminen epäonnistui!");
     return;
   }
 
-  // Construct the data string
-  String data = "temperature=" + String(temperature) + "&humidity=" + String(humidity);
-  Serial.print("Sending data: ");
+  // Muodostetaan datajoukko merkkijonona
+  String data = "lämpötila=" + String(temperature) + "&ilman_kosteus=" + String(humidity);
+  Serial.print("Lähetetään dataa: ");
   Serial.println(data);
 
-  // Send HTTP POST request to WAMP server
+  // Lähetetään HTTP POST -pyyntö WAMP-palvelimelle
   HTTPClient http;
   http.begin("http://" + String(serverIP) + String(endpoint));
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   int httpResponseCode = http.POST(data);
 
   if (httpResponseCode > 0) {
-    Serial.print("HTTP Response code: ");
+    Serial.print("HTTP-vastauskoodi: ");
     Serial.println(httpResponseCode);
   } else {
-    Serial.print("Error in HTTP request: ");
+    Serial.print("Virhe HTTP-pyynnössä: ");
     Serial.println(httpResponseCode);
   }
 
   http.end();
 
-  delay(10000);  // Wait for 1 second before sending the next request
+  delay(10000);  // Odota 10 sekuntia ennen seuraavaa pyyntöä
 }
