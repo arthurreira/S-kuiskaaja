53aac605f4954157469a29dbeb954ffe0e79b090
Arthur Ferreira <87419255+Naroshh@users.noreply.github.com>
2024-03-24 20:05:06 +0200

    Update sketch_bme280.ino
    suomeksi

--- Esp32 arduivno IDE/sketch_bme280/sketch_bme280.ino
+++ Esp32 arduivno IDE/sketch_bme280/sketch_bme280.ino
@@ -1,84 +1,77 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#include <Wire.h>
+#include <WiFi.h>               // WiFi-kirjasto ESP32:lle
+#include <HTTPClient.h>         // HTTP-asiakaskirjasto HTTP-pyynnön tekemiseen
+#include <Adafruit_Sensor.h>    // Anturikirjasto BME280-anturia varten
+#include <Adafruit_BME280.h>    // BME280-anturikirjasto
+#include <Wire.h>               // I2C-kommunikaatiokirjasto
 
-const char* ssid = "mywifi";      // Enter your WiFi SSID here
-const char* password = "mypassword";  // Enter your WiFi password here
+const char* ssid = "mywifi";        // WiFi-verkkosi SSID
+const char* password = "mypassword"; // WiFi-verkkosi salasana
 
-// BME280 Sensor
-#define BME_SDA 23
-#define BME_SCL 22
-#define SEALEVELPRESSURE_HPA (1013.25)     // Adjust this to your local sea level pressure
+// BME280-anturi
+#define BME_SDA 23                  // BME280:n SDA-pinni
+#define BME_SCL 22                  // BME280:n SCL-pinni
+#define SEALEVELPRESSURE_HPA (1013.25) // Säädä tämä paikallisen merenpinnan paineen mukaan
 
-Adafruit_BME280 bme;
+Adafruit_BME280 bme;                // Luodaan BME280-anturin objekti
 
-const char* serverIP = "xxx.xxx.xxx.xxx";   // Use the IP of your WAMP server
-const char* endpoint = "/Weather-app-IOT/insert_data_bme280.php";  // Endpoint for the PHP script
-
-#define LED_PIN 13  // LED pin
+const char* serverIP = "xxx.xxx.xxx.xxx";   // WAMP-palvelimesi IP-osoite
+const char* endpoint = "/Weather-app-IOT/insert_data_bme280.php"; // PHP-skriptin loppupiste
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT); // Initialize LED pin
-  Wire.begin(BME_SDA, BME_SCL);
+  Serial.begin(115200);           // Alustetaan sarjaportti
 
+  // Alustetaan BME280-anturi
+  Wire.begin(BME_SDA, BME_SCL);
   if (!bme.begin()) {
-    Serial.println("Could not find BME280 sensor, check wiring!");
+    Serial.println("BME280-anturia ei löydy, tarkista kytkentä!");
     while (1);
   }
 
-  Serial.println("Connecting to ");
+  // Yhdistetään WiFi-verkkoon
+  Serial.println("Yhdistetään verkkoon ");
   Serial.println(ssid);
-
-  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED_PIN, LOW); // Turn off LED if not connected
-    delay(500);
-    digitalWrite(LED_PIN, HIGH); // Turn on LED if connected
     delay(500);
     Serial.print(".");
   }
-  digitalWrite(LED_PIN, HIGH); // Turn off LED if connected
   Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");
+  Serial.println("WiFi-yhteys muodostettu..!");
+  Serial.print("Saimme IP:n: ");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
+  // Luetaan ympäristötiedot BME280-anturilta
   float temperature = bme.readTemperature();
   float humidity = bme.readHumidity();
-  float pressure = bme.readPressure() / 100.0F; // Pressure in hPa
+  float pressure = bme.readPressure() / 100.0F; // Ilmanpaine hehtopascaleissa
   float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
 
-  Serial.print("Temperature = ");
+  // Tulostetaan tiedot sarjaporttiin
+  Serial.print("Lämpötila = ");
   Serial.print(temperature);
   Serial.println(" *C");
 
-  Serial.print("Humidity = ");
+  Serial.print("Ilmankosteus = ");
   Serial.print(humidity);
   Serial.println(" %");
 
-  Serial.print("Pressure = ");
+  Serial.print("Ilmanpaine = ");
   Serial.print(pressure);
   Serial.println(" hPa");
 
-  Serial.print("Altitude = ");
+  Serial.print("Korkeus = ");
   Serial.print(altitude);
-  Serial.println(" meters");
+  Serial.println(" metriä");
 
-  // Construct the data string
-  String data = "temperature=" + String(temperature) + "&humidity=" + String(humidity) + "&pressure=" + String(pressure) + "&altitude=" + String(altitude);
+  // Luodaan datajoukko merkkijonona
+  String data = "lämpötila=" + String(temperature) + "&ilman_kosteus=" + String(humidity) + "&ilmanpaine=" + String(pressure) + "&korkeus=" + String(altitude);
 
-
-
-  Serial.print("Sending data: ");
+  Serial.print("Lähetetään dataa: ");
   Serial.println(data);
 
-  // Send HTTP POST request to WAMP server
+  // Lähetetään HTTP POST -pyyntö WAMP-palvelimelle
   HTTPClient http;
   http.begin("http://" + String(serverIP) + String(endpoint));
 
@@ -86,22 +79,19 @@ void loop() {
   int httpResponseCode = http.POST(data);
 
   if (httpResponseCode > 0) {
-    Serial.print("HTTP Response code: ");
+    Serial.print("HTTP-vastauskoodi: ");
     Serial.println(httpResponseCode);
-    digitalWrite(LED_PIN, HIGH); // Turn on LED
-    delay(500); // Delay for LED blinking
-    digitalWrite(LED_PIN, LOW); // Turn off LED
   } else {
-    Serial.print("Error in HTTP request: ");
+    Serial.print("Virhe HTTP-pyynnössä: ");
     Serial.println(httpResponseCode);
   }
   if (httpResponseCode == 200) {
     String responseBody = http.getString();
-    Serial.println("Received response:");
+    Serial.println("Vastaanotettu vastaus:");
     Serial.println(responseBody);
   }
 
   http.end();
 
-  delay(10000);  // Wait for 10 seconds before sending the next request
+  delay(10000);  // Odota 10 sekuntia ennen seuraavaa pyyntöä
 }
